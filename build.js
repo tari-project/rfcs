ead2b4d8096490e8cb56108e6cd548364b79970b
Cayle Sharrock <CjS77@users.noreply.github.com>
2024-03-06 10:33:35 +0000

    feat: node-based build script (#131)
    Add a node.js-based build script so that this site can run in Cloudflare
    pages.
    
    The script does the following:
    1. Downloads the specified version of mdbook from the GitHub releases
    page
    2. Verifies its SHA256 hash
    3. Extracts it to a temporary directory
    4. Runs mdbook build against the book.toml file in the current directory
    5. Places the built book in the temporary directory
    
    `mdbook.env` (which is checked in and MUST NOT hold any secrets) allows
    you to set the version of mdbook and its hash.

---
+++ build.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+require('dotenv').config({ path: './mdbook.env'});
+const axios = require('axios');
+const fs = require('fs');
+const fsp = fs.promises;
+const crypto = require('crypto');
+const { exec } = require('child_process');
+const tar = require('tar');
+
+const VERSION = process.env.MDBOOK_VERSION || '0.4.37'; // replace with the version you want
+const MDBOOK_HASH = process.env.MDBOOK_HASH;
+const url = `https://github.com/rust-lang/mdBook/releases/download/v${VERSION}/mdbook-v${VERSION}-x86_64-unknown-linux-gnu.tar.gz`;
+
+async function downloadFile(url, path) {
+    const writer = fs.createWriteStream(path);
+    const response = await axios({ url, method: 'GET', responseType: 'stream' });
+    response.data.pipe(writer);
+    return new Promise((resolve, reject) => {
+        writer.on('finish', resolve);
+        writer.on('error', reject);
+    });
+}
+
+function calculateHash(path) {
+    return new Promise((resolve, reject) => {
+        const hash = crypto.createHash('sha256');
+        const stream = fs.createReadStream(path);
+        stream.on('data', (data) => hash.update(data));
+        stream.on('end', () => resolve(hash.digest('hex')));
+        stream.on('error', reject);
+    });
+}
+
+async function main() {
+    const buildPath = './build';
+    try {
+        await fsp.mkdir(buildPath, { recursive: true })
+    } catch (e) {
+        console.error(`👎️ Could not create build directory ${e}`);
+        process.exit(2);
+    }
+    const zipLoc = `${buildPath}/mdbook.tar.gz`;
+    await downloadFile(url, zipLoc);
+    const hash = await calculateHash(zipLoc);
+    if (hash !== MDBOOK_HASH) {
+        console.error(`👎️ Hash mismatch. expected ${MDBOOK_HASH} but got ${hash}`);
+        process.exit(1);
+    }
+    console.log(`👍️ MDBook release hash matches ${hash}`);
+    await tar.x({ file: zipLoc, C: buildPath });
+    exec(`${buildPath}/mdbook build -d ./book`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`👎️ exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+    });
+}
+
+main().catch(console.error);
