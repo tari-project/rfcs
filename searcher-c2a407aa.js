97135fa78c3c7d1bc660f2da5da072f55f94f934
SWvheerden <swvheerden@gmail.com>
2026-07-17 08:33:15 +0000

    [skip ci] deploy gh-pages

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d67ce520.js',
+                path_to_root + 'searchindex-6ead775a.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
